11d7fd4264581aabc2ca88942ddbcc9d86c550b7
Vova Babushkin <vababushkin@edu.hse.ru>
2024-09-23 23:39:33 +0300

    fulll p part

--- BDZ1/p5.cpp
+++ BDZ1/p5.cpp
@@ -1,56 +1,51 @@
+#include <deque>
 #include <iostream>
-#include <list>
+#include <stack>
+#include <vector>
 
-int main(){
-    //ios::sync_with_stdio(false);  // Отключить синхронизацию между iostream и stdio.
-    std::cin.tie(nullptr);        // Отключить синхронизацию между std::cin и std::cout.
+int main() {
 
-    std::list<int> l;
-    auto mid = l.end();
+  std::ios::sync_with_stdio(false); // Отключить синхронизацию между iostream и stdio.
+  std::cin.tie(nullptr); // Отключить синхронизацию между std::cin и std::cout
 
-    int times;
-    std::cin>>times;
-    while(times--){
-        char c;
-        std::cin>>c;
-        if(c=='+'){
-            int n;
-            std::cin>>n;
-            l.push_back(n);
-            if(l.size()==1){
-                mid = l.begin();
-            }
-            else if(l.size()%2 || l.size() == 2){
-                ++mid;
-            }
-        }
-        else if(c=='*'){
-            int n;
-            std::cin>>n;
-            if(l.size()<2){
-                l.push_back(n);
-            }
-            else
-                l.insert(mid,n);
+  std::deque<int> front;
+  std::deque<int> back;
 
-            if(l.size()==1){
-                mid = l.begin();
-            }
-            else if(l.size()%2==0){
-                --mid;
-            }
-        }
-        else{
-            std::cout<<l.front()<<'\n';
-            l.pop_front();
-            if(l.size()%2){
-                ++mid;
-            }
-        }
+  int times;
+  std::cin >> times;
 
-        // for(auto x:l){
-        //     std::cout<<x<<' ';
-        // }std::cout<<"   "<<*mid<<'\n';
+  while(times--) {
 
+    char c;
+    std::cin >> c;
+
+    if (c == '+') {
+      int n;
+      std::cin >> n;
+      back.push_back(n);
+
+    } else if (c == '*') {
+      int n;
+      std::cin >> n;
+      back.push_front(n);
+
+    } else if (c == '-') {
+
+      if (!front.empty()) {
+        std::cout << front.front() << '\n';
+        front.pop_front();
+
+      } else {
+        std::cout << back.front() << '\n';
+        back.pop_front();
+      }
     }
+
+    if (front.size() < back.size()) {
+      front.push_back(back.front());
+      back.pop_front();
+    }
+  }
+  
+  return 0;
 }
