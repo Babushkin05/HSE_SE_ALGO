615760486b14450e2dcb7486c3d6f698e3618d56
Vova Babushkin <vababushkin@edu.hse.ru>
2024-09-28 15:30:56 +0300

    a5 pril

---
+++ BDZ1/A5.cpp
@@ -0,0 +1,29 @@
+#include <vector>
+
+std::pair<int,int> index_by_key(std::vector<std::vector<int>> matrix, int n, int k){
+    for(size_t i = 0; i < n; ++i){
+        if(matrix[i][i] == k){
+            return std::make_pair(i,i);
+        }
+        if(matrix[i][i] > k){
+            for(size_t j = i - 1; j >= 0; --j){
+                if(matrix[i][j] == k){
+                    return std::make_pair(i,j);
+                }
+                if(matrix[i][j] < k){
+                    break;
+                }
+            }
+
+            for(size_t j = i - 1; j >= 0; --j){
+                if(matrix[j][i] == k){
+                    return std::make_pair(j,i);
+                }
+                if(matrix[j][i] < k){
+                    break;
+                }
+            }
+        }
+    }
+    return std::make_pair(-1,-1);
+}
