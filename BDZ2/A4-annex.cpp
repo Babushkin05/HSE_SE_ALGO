c001a0ba63538f0d3131d5b9c8f899e65846c502
Vova Babushkin <vababushkin@edu.hse.ru>
2024-10-11 02:07:08 +0300

    add assets

---
+++ BDZ2/A4-annex.cpp
@@ -0,0 +1,82 @@
+#include <vector> 
+#include <iostream>
+
+
+std::vector<int> v;
+
+void merge(int l,int r){
+    int m = (l+r)/2;
+    int n1 = m-l;
+    int n2 = r-m;
+    std::vector<int> v1(n1,0), v2(n2,0);
+
+    for (int i = 0; i < n1; ++i)
+        v1[i] = v[l + i];
+    for (int j = 0; j < n2; ++j)
+        v2[j] = v[m + j];
+
+    int i = 0;
+    int j = 0;
+    int k = l;
+    while (i < n1 && j < n2) {
+        if (v1[i] < v2[j]) {
+            v[k] = v1[i];
+            ++i;
+        } else {
+            v[k] = v2[j];
+            ++j;
+        }
+        ++k;
+    }
+    while (i < n1) {
+        v[k] = v1[i];
+        ++i;
+        ++k;
+    }
+    while (j < n2) {
+        v[k] = v2[j];
+        ++j;
+        ++k;
+    }
+}
+
+int count_inversions(int l, int r){
+    int res = 0;
+    int m = (l+r)/2;
+    int i = l;
+    int j = m;
+    while(i < m && j < r){
+        if(v[i]>v[j]){
+            res += j - m + 1;
+            ++j;
+        }
+        else{
+            ++i;
+        }
+    }
+
+    return res;
+}
+
+int extended_merge_sort(int l, int r){
+    if(l+1>=r){ 
+        return 0;
+    }
+    int m = (l+r)/2;
+    int ans = 0;
+    ans += extended_merge_sort(l,m);
+    ans += extended_merge_sort(m,r);
+    ans += count_inversions(l,r);
+    merge(l,r);
+    return ans;
+}
+
+int main() {
+    int n;
+    std::cin>>n;
+    v.resize(n);
+    for(size_t i = 0; i< n;++i){
+        std::cin>>v[i];
+    }
+    std::cout<<extended_merge_sort(0,n);
+}
