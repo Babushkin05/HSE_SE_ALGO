b184931ce535a1bf066059db8243dc8a386fef95
Vova Babushkin <babushkinvova05@gmail.com>
2024-11-21 18:32:15 +0300

    a2i

---
+++ BDZ3/A2/A2i.cpp
@@ -0,0 +1,85 @@
+#include <algorithm>
+#include <iostream>
+#include <vector>
+
+std::vector<int> v;
+
+void merge(int l, int r) {
+  int m = (l + r) / 2;
+  int n1 = m - l;
+  int n2 = r - m;
+  std::vector<int> v1(n1), v2(n2);
+
+  for (int i = 0; i < n1; ++i)
+    v1[i] = v[l + i];
+  for (int j = 0; j < n2; ++j)
+    v2[j] = v[m + j];
+
+  int i = 0;
+  int j = 0;
+  int k = l;
+  while (i < n1 && j < n2) {
+    if (v1[i] <= v2[j]) {
+      v[k] = v1[i];
+      ++i;
+    } else {
+      v[k] = v2[j];
+      ++j;
+    }
+    ++k;
+  }
+  while (i < n1) {
+    v[k] = v1[i];
+    ++i;
+    ++k;
+  }
+  while (j < n2) {
+    v[k] = v2[j];
+    ++j;
+    ++k;
+  }
+}
+
+void insertion_sort(int l, int r) {
+  for (size_t i = l; i < r; ++i) {
+    int k = v[i];
+    int j = i - 1;
+    while (j >= l && v[j] > k) {
+      v[j + 1] = v[j];
+      --j;
+    }
+    v[j + 1] = k;
+  }
+}
+
+void merge_sort(int l, int r) {
+  if (l + 1 >= r) {
+    return;
+  }
+  if (r - l <= 15) {
+    insertion_sort(l, r);
+    return;
+  }
+  int m = (l + r) / 2;
+  merge_sort(l, m);
+  merge_sort(m, r);
+  merge(l, r);
+  return;
+}
+
+int main() {
+  // Отключить синхронизацию между iostream и stdio.
+  std::ios::sync_with_stdio(false);
+  // Отключить синхронизацию между std::cin и std::cout.
+  std::cin.tie(nullptr);
+  int n;
+  std::cin >> n;
+  v.resize(n);
+  for (size_t i = 0; i < n; ++i) {
+    std::cin >> v[i];
+  }
+  merge_sort(0, n);
+  for (size_t i = 0; i < n; ++i) {
+    std::cout << v[i] << ' ';
+  }
+}
