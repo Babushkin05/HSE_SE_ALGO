e355ab25723b57d0c2a5c79e5ac473ac9db7f59f
Vova Babushkin <babushkinvova05@gmail.com>
2024-12-09 15:51:45 +0300

    p2

--- BDZ4/P2.cpp
+++ BDZ4/P2.cpp
@@ -41,13 +41,13 @@ bool check_red_black(int num, bool must_be_black = 1) {
   bool f = check_red_black(tree[num].left, tree[num].color == 'R') &&
            check_red_black(tree[num].right, tree[num].color == 'R');
   f &= !must_be_black || (tree[num].color == 'B');
-  int lefth =
-      tree[tree[num].left].blackheight + static_cast<int>(tree[tree[num].left].color == 'B');
-  int righth =
-      tree[tree[num].right].blackheight + static_cast<int>(tree[tree[num].right].color == 'B');
+  int lefth = tree[tree[num].left].blackheight +
+              static_cast<int>(tree[tree[num].left].color == 'B');
+  int righth = tree[tree[num].right].blackheight +
+               static_cast<int>(tree[tree[num].right].color == 'B');
   f &= lefth == righth;
-  tree[num].blackheight = lefth;
-  std:: cout<<tree[num].key <<' '<<lefth<<' '<<righth<<'\n';
+  tree[num].blackheight = std::min(righth, lefth);
+  // std:: cout<<tree[num].key <<' '<<lefth<<' '<<righth<<'\n';
   f &= tree[num].color == 'B' || tree[num].color == 'R';
   f &= (tree[num].right == 0 || tree[num].key < tree[tree[num].right].key) &&
        (tree[num].left == 0 || tree[num].key > tree[tree[num].left].key);
@@ -55,13 +55,24 @@ bool check_red_black(int num, bool must_be_black = 1) {
 }
 
 int main() {
+  // Отключить синхронизацию между iostream и stdio.
+  std::ios::sync_with_stdio(false);
+  // Отключить синхронизацию между std::cin и std::cout.
+  std::cin.tie(nullptr);
   int n, root;
-  std::cin >> n >> root;
+  std::cin >> n;
+  if (n == 0) {
+    std::cout << "NO";
+    return 0;
+  }
+  std::cin >> root;
   tree.resize(n + 1);
   tree[0].blackheight = -1;
   tree[0].color = 'B';
   for (size_t i = 1; i <= n; ++i) {
-    std::cin >> tree[i];
+    node a;
+    std::cin >> a;
+    tree[a.number] = a;
   }
   bool f = check_red_black(root);
   if (f) {
