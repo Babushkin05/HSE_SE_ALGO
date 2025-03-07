29bc4eca48aa79527c7d0ff9c7283e185b86edef
Vova Babushkin <babushkinvova05@gmail.com>
2025-03-07 15:05:29 +0300

    add test din

--- BDZ7/dinic.cpp
+++ BDZ7/dinic.cpp
@@ -67,3 +67,57 @@ public:
         return maxFlow;
     }
 };
+
+enum Vertex {
+    S,  
+    A,  
+    B,  
+    C, 
+    D,  
+    E,  
+    F, 
+    G,  
+    H,  
+    I,  
+    T   
+};
+
+int main() {
+    int n = 12; 
+    Network network(n);
+
+    network.addEdge(S, A, 5);
+    network.addEdge(S, D, 10); 
+    network.addEdge(S, G, 15);  
+
+    network.addEdge(A, B, 10);
+
+    network.addEdge(B, E, 25); 
+    network.addEdge(B, C, 10); 
+
+    network.addEdge(C, T, 5); 
+
+    network.addEdge(D, E, 20); 
+
+    network.addEdge(E, G, 5); 
+    network.addEdge(E, F, 30); 
+
+    network.addEdge(F, I, 15);
+    network.addEdge(F, T, 15); 
+
+    network.addEdge(G, H, 25);
+
+    network.addEdge(H, F, 20); 
+    network.addEdge(H, I, 10);
+
+    network.addEdge(I, T, 10); 
+
+    int source = S; 
+    int sink = T;  
+
+    long long maxFlow = network.dinic(source, sink);
+
+    cout << "Max Flow: " << maxFlow << endl;
+
+    return 0;
+}
